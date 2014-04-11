7260b335fc2b86631426a9a9979c760c25c538e6
Daniel Zamorano <daniel.zamorano@wisertogether.com>
2014-04-11 15:12:33 -0500

    closes #1 - Contact Page fully working, logo changes to home.

--- scripts/app.js
+++ scripts/app.js
@@ -22,6 +22,10 @@ theAutoBodyShopApp.config(['$routeProvider', function($routeProvider) {
       templateUrl: 'partials/home.html',
       controller: 'homeController'
     }).
+    when('/contact', {
+      templateUrl: 'partials/contact.html',
+      controller: 'contactController'
+    }).
     otherwise({
       redirectTo: '/home'
     });
@@ -41,4 +45,95 @@ theAutoBodyShopApp.controller('homeController', ['$scope', '$http',
         backgrounds: $scope.backgrounds
       })('overlay');
     });
+  }]);
+
+// CONTACT PAGE
+theAutoBodyShopApp.controller('contactController', ['$scope', '$http',
+  function ($scope, $http) {
+    $.vegas('stop')
+      ({
+        src:'/images/home-slider/FORDMATTE.jpg'
+      })('overlay');
+
+    $scope.emailForm = {};
+
+    $scope.cleanForm = function() {
+      $scope.emailForm = {};
+    };
+
+    $scope.generateMessage = function(emailData) {
+      var
+      name = "Nombre: " + emailData.name,
+      email = "Email: " + emailData.email,
+      telefono = "Teléfono: " + emailData.phone,
+      mensaje = "Mensaje: " + emailData.message,
+      sign = "Legion | The Auto Body Shop | Web Form ",
+      lineBreak = {
+        html:"<br/>",
+        text: "\n"
+      },
+
+      htmlEmailTemplate = "<p>" + name + lineBreak.html + email + lineBreak.html +
+      telefono + lineBreak.html + "</p>" + "<p>"+ mensaje + "</p>" + "<p>" + sign +"</p>",
+
+      textEmailTemplate = lineBreak.text +
+        name + lineBreak.text + email + lineBreak.text + telefono + lineBreak.text +
+        lineBreak.text + mensaje + lineBreak.text +
+        lineBreak.text + sign + lineBreak.text;
+
+      return {
+        "html": htmlEmailTemplate,
+        "text": textEmailTemplate
+      };
+    };
+
+
+    $scope.generateEmail = function(emailData) {
+      var testKey = 'IUNZeo_PpN26eaINR5HDrw',
+      realKey = 'LGI6bAvlRP9LBKpYjwKGFg',
+      message = $scope.generateMessage(emailData);
+
+      return {
+        "key": realKey,
+        "message": {
+          "html": message.html || undefined,
+          "text": message.text || undefined,
+          "subject": "The Auto Body Shop | Web Form",
+          "from_email": $scope.emailForm.email,
+          "from_name": $scope.emailForm.name,
+          "to": [
+            {
+              "email": "ventas@theautobodyshop.mx",
+              "name": "Ventas | The Auto Body Shop",
+              "type": "to"
+            }
+          ],
+          "headers": {
+            "Reply-To": $scope.emailForm.email
+          }
+        }
+      };
+    };
+
+    $scope.sendEmail = function(emailData) {
+      if(emailData.$invalid) {
+        return;
+      }
+
+      var email = $scope.generateEmail(emailData);
+
+      $http.post('https://mandrillapp.com/api/1.0/messages/send.json', email).
+      success(function(data, status, headers, config) {
+        if(data[0].status === 'error' || data[0].status === 'rejected') {
+          $('.alert.alert-danger').show();
+        }
+        else if(data[0].status === 'sent') {
+          $('.alert.alert-success').show();
+        }
+      }).
+      error(function(data, status, headers, config) {
+        $('.alert.alert-danger').show();
+      });
+    };
+
   }]);
